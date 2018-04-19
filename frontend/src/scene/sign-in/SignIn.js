ac5e6b90baacddfd84584517846e7ec37a3f6da8
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-04-19 14:39:28 +0300

    Added app Headline

--- frontend/src/scene/sign-in/SignIn.js
+++ frontend/src/scene/sign-in/SignIn.js
@@ -28,15 +28,29 @@ const LogoSection = styled('div')`
     flex-direction: column;
     margin-top: 3rem;
     filter: drop-shadow(0 6px 12px rgba(0, 0, 0, 0.23));
+
+    div {
+        flex: inherit;
+        align-items: center;
+        text-align: center;
+    }
 `;
 const AppName = styled('h1')`
     text-transform: uppercase;
     font-size: 4rem;
-    letter-spacing: 4px;
     font-weight: bold;
     color: white;
     margin: 0;
+    letter-spacing: 4px;
 `;
+
+const AppHeadLine = styled('span')`
+    font-size: 2.3rem;
+    color: white;
+    opacity: 0.8;
+    letter-spacing: 2px;
+`;
+
 const SizedLogo = styled(Logo)`
     width: 15rem;
     height: 15rem;
@@ -57,9 +71,14 @@ class SignIn extends React.Component {
             <StyledWrapper>
                 <LogoSection>
                     <SizedLogo />
-                    <AppName>
-                        {this.props.ContentStore.content.global.appName}
-                    </AppName>
+                    <div>
+                        <AppName>
+                            {this.props.ContentStore.content.global.appName}
+                        </AppName>
+                        <AppHeadLine>
+                            {this.props.ContentStore.content.global.appHeadLine}
+                        </AppHeadLine>
+                    </div>
                 </LogoSection>
                 <LoginForm />
                 <FixedDecorator />,
