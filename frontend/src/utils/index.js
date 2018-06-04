e1219f41806bc0fa369e80b1ef81bcb3c7dce95a
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-06-04 12:39:20 +0300

    Fix style ugliness in small modal in ResetPin & Registration. Add comma separator instead of period for localization. Tweak animation in various places. Added links and manifests for PWA on iOS. Tweak validation logic in balance modal.

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -62,3 +62,7 @@ export const validateUsername = (username) => {
     // no contrains now for username
     return true;
 };
+
+export const composeFunction = (...funcList) => (args) => {
+    funcList.forEach((func) => func(args));
+};
